d0cb1af3b9ee30b90ede9377dee98d3b0bbccff1
sangil park <wolf5839@gmail.com>
2023-02-19 12:13:50 +0900

    ８章追加

---
+++ ZKNOTE/app/Policies/Article.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Article;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ArticlePolicy
+{
+    use HandlesAuthorization;
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    public function view(User $user, Article $article)
+    {
+        //
+    }
+
+    public function create(User $user)
+    {
+        //
+    }
+
+    public function update(User $user, Article $article)
+    {
+        return $user->id == $article->user_id;
+    }
+
+    public function delete(User $user, Article $article)
+    {
+        return $user->id == $article->user_id;
+    }
+
+    public function restore(User $user, Article $article)
+    {
+        //
+    }
+
+    public function forceDelete(User $user, Article $article)
+    {
+        //
+    }
+}
